a4196d1f925b6a58157cadc1e2cb75617b861c0d
미플 <devmeeple@gmail.com>
2023-09-06 23:59:57 +0900

    chore(mongodb): 몽고DB 연결

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://devmeeple:mongodb3485@cluster0.sc7iabh.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('MongoDB Connected...')) //
+    .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
