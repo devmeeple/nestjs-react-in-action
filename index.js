dafbf9653446c9441ef39e0bbd5f14c91f651d0f
미플 <devmeeple@gmail.com>
2023-09-07 04:12:04 +0900

    refactor(mongodb): 몽고DB 비밀 설정 정보 관리
    프로퍼티 파일에 설정 정보를 적어두고 관리하듯이 분리하는 작업을 진행하였다.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded
@@ -11,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://devmeeple:mongodb3485@cluster0.sc7iabh.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
     .then(() => console.log('MongoDB Connected...')) //
     .catch(err => console.log(err))
 
